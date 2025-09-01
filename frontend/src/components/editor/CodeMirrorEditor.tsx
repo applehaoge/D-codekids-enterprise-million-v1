03243d3ca752e8816a0768b8601c3b979ad43aba
orangehao_bit <applehaoge@gmail.com>
2025-09-01 14:51:16 +0800

    fix: 调整 CodeMirrorEditor 高度避免 console 区被撑大

--- frontend/src/components/editor/CodeMirrorEditor.tsx
+++ frontend/src/components/editor/CodeMirrorEditor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2c04965605bc27310eb8c1f536a855445c71b42b249034bfebf15d6ab0085ae
-size 3226
+oid sha256:74a14826f912ceddbfa8854f882cc461a42787fbdf31ded9c14dedefc0a9c7de
+size 4126
