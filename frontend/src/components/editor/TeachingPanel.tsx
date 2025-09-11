c13dee644937fd58a0e640c8be21765572f6bdda
orangehao_bit <applehaoge@gmail.com>
2025-09-11 14:25:10 +0800

    chore: 保存 TeachingPanel 放大模式计划 v1

--- frontend/src/components/editor/TeachingPanel.tsx
+++ frontend/src/components/editor/TeachingPanel.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29fcfd430c7aaff1438abb8be3a052296ce15ca6eb950aad5ea4a9ea9db59fda
-size 7741
+oid sha256:c2ce54e94fc4afb042568949303f586f4d8ea74727cd606c7d4db81c47575195
+size 10724
--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adeebdc07859c564dfcafa7771fc23c23a3e96182a107499451c38f9609c7262
-size 9638
+oid sha256:e6598fa28041bcb612a9c5b384e2bf8de336096c5eba9e80bca29ddb64b9c029
+size 9708
